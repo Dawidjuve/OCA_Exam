cb69e29371cc27c1c2c5b416ff5976e52da8f534
Dawid Honorowicz <dhonorowicz@opsenio.com>
2019-07-22 17:02:43 +0200

    Ukończony rozdział 2

---
+++ src/ForLoop.java
@@ -0,0 +1,76 @@
+
+public class ForLoop {
+
+	public ForLoop() {
+	}
+
+	public static void main(String[] args) {
+
+//		for(int a =2, int b = 3, int c =4; a<c; a++) {
+//			
+//		}
+
+		for (int a = 2, b = 3, c = 4; a < c; a++, b--) {
+
+		}
+
+//		for(;;);
+//		for(;);
+//		for();
+
+		int g = 0;
+//		for (long f = 0, g = 4; g < 5 && f < 10; g++, f++) { // DOES NOT COMPILE
+//			System.out.print(g + " ");
+//		}
+
+		// Powy¿szy przyk³ad mo¿na naprawiæ nastêpuj¹co:
+		long f;
+		for (f = 0, g = 4; g < 5 && f < 10; g++, f++) { // DOES NOT COMPILE
+			System.out.print(g + " ");
+
+		}
+
+		for (int i = 2; i < 4; i++) {
+			System.out.println(i);
+		}
+//		System.out.println(i);
+
+		int h;
+		for (h = 0; h < 2; h++) {
+			System.out.println("W pêtli: " + h);
+		}
+		System.out.println("Za pêtl¹: " + h);
+
+		java.util.List<String> names = new java.util.ArrayList<String>();
+		names.add("Lisa");
+		names.add("Kevin");
+		names.add("Roger");
+		for (int i = 0; i < names.size(); i++) {
+			String name = names.get(i);
+			if (i > 0) {
+				System.out.print(", ");
+			}
+			System.out.print(name);
+		}
+
+		sINGLE_LOOP
+
+		: for (int i = 1; i > 2; i++) {
+
+		}
+
+		single_while: do
+			System.out.println("single while");
+		while (false);
+
+		FIRST_CHAR_LOOP: for (int a = 1; a <= 4; a++) {
+			for (char x = 'a'; x <= 'c'; x++) {
+				if (a == 2 || x == 'b')
+					continue FIRST_CHAR_LOOP;
+				System.out.print(" " + a + x);
+			}
+		}
+
+	}
+
+}
---
+++ src/Statements.java
@@ -0,0 +1,237 @@
+
+public class Statements {
+
+	public Statements() {
+	}
+
+	public static void main(String[] args) {
+
+		boolean a = true;
+		if (a == true)
+
+			System.out.println("a");
+
+		///
+
+		if (a == false)
+
+			System.out.println("b");
+
+		else
+
+			System.out.println("else b");
+
+		////
+
+//		if(a==false)
+//
+//			System.out.println("b");
+//			System.out.println("another line ");
+//
+//		else {
+//			System.out.println("else b");
+
+		//////
+
+		int hourOfDay = 10;
+		if (hourOfDay < 15) {
+			System.out.println("Good Afternoon");
+		} else if (hourOfDay < 11) {
+			System.out.println("Good Morning"); // UNREACHABLE CODE BUT IT WILL COMPILE
+		} else {
+			System.out.println("Good Evening");
+		}
+
+		/////////////////////////
+
+//		int x = 1;
+//		if(x = 5) { // DOES NOT COMPILE
+//		
+//		}
+		
+		
+		////
+		
+		
+		if(a==true)
+			System.out.println("1"); System.out.println("2");System.out.println("3");
+
+		/////
+		int y = 4;
+		System.out.println((y > 5) ? 21 : "Zebra");
+//		int animal = (y < 91) ? 9 : "Horse"; // DOES NOT COMPILE
+
+		/////
+
+		int c = 1;
+		int d = 1;
+		final int e = c < 10 ? c++ : d++;
+		System.out.println("c= " + c + "," + "d= " + d + ",e = " + e);
+
+		/////
+		final int f = 5;
+		;
+		int dayOfWeek = 5;
+		switch (dayOfWeek) {
+		default:
+			System.out.println("Weekday");
+			break;
+		case 0:
+			System.out.println("Sunday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case f:
+			System.out.println("f");
+		}
+
+		////
+
+		switch (dayOfWeek) {
+
+		case (int) 0.0:
+			System.out.println("Sunday");
+			break;
+		}
+
+		///
+
+		final int g = 2;
+		short dayOfWeek2 = 5;
+		switch (dayOfWeek2) {
+		default:
+			System.out.println("Weekday");
+			break;
+		case g:
+			System.out.println("Sunday");
+			break;
+		}
+
+		////
+
+//		int h = 4;
+//		switch (dayOfWeek) {
+//		default:
+//			System.out.println("Weekday");
+//			break;
+//		case h:
+//			System.out.println("Sunday");
+//			break;
+//		}
+
+		///
+		System.out.println("-----------------");
+
+		int dayOfWeek3 = 5;
+		switch (dayOfWeek3) {
+		case 0:
+			System.out.println("Sunday");
+		default:
+			System.out.println("Weekday");
+//			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		}
+
+		///
+
+		System.out.println("-----------------dayOfWeek4");
+
+		int dayOfWeek4 = 5;
+		switch (dayOfWeek4) {
+		case 5:
+			System.out.println("Sunday");
+			break;
+		default:
+			System.out.println("Weekday");
+//			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		}
+
+		///
+
+		System.out.println("-----------------dayOfWeek5");
+
+		int dayOfWeek5 = 5;
+		switch (dayOfWeek5) {
+
+		default:
+			System.out.println("Weekday");
+//			break;
+		case 5:
+			System.out.println("Sunday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		}
+	
+	
+	
+	/////
+	
+	System.out.println("-----------------dayOfWeek6");
+	
+	int dayOfWeek6 = 5;
+	switch (dayOfWeek6) {
+	
+	}
+	
+	///////
+	
+//	do {
+//		System.out.println("ssss");
+//	}while();
+	
+	
+	/////
+	int h = 1;
+	do
+		System.out.println("ss");
+		
+		while(false);
+	
+	
+	}
+
+//	private int getSortOrder(String firstName, final String lastName) {
+//		String middleName = "Patricia";
+//		final String suffix = "JR";
+//		int id = 0;
+//		switch (firstName) {
+//		case "Test":
+//			return 52;
+//
+//		case middleName: // DOES NOT COMPILE
+//			id = 5;
+//			break;
+//		case suffix:
+//			id = 0;
+//			break;
+//		case lastName: // DOES NOT COMPILE
+//			id = 8;
+//			break;
+//
+//		}
+//		return id;
+//	}
+
+//	private int getSortOrder(String firstName, final String lastName) {
+//		String middleName = "Patricia";
+//		final String suffix = "JR";
+//		int id = 0;
+//		switch (firstName) {
+//		case 'J': // DOES NOT COMPILE
+//			id = 10;
+//			break;
+//		case java.time.DayOfWeek.SUNDAY: // DOES NOT COMPILE
+//			id = 15;
+//			break;
+//		}
+//		return id;
+//	}
+}
